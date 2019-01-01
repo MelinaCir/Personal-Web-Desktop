1f0be784e5896f61d610c4d5b0d2de28aff20d23
Melina Cirverius <mc222ap@student.lnu.se>
2019-01-01 16:38:38 +0100

    Adding functionality to move windows

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
-let dragWindow = document.querySelector('#moveme')
-let container = document.querySelector('#outer')
+let dragWindow = document.querySelector('#moveheader')
+let container = document.querySelector('#moveme')
 
 let active = false
 let currentX
@@ -9,40 +9,73 @@ let initialY
 let offsetX = 0
 let offsetY = 0
 
-container.addEventListener('mousedown', dragStart, false)
-container.addEventListener('mouseup', dragEnd, false)
+container.addEventListener('mousedown', startMove, false)
+container.addEventListener('mouseup', stopMove, false)
+container.addEventListener('mouseleave', stopMove, false)
 container.addEventListener('mousemove', drag, false)
 
-function dragStart (e) {
-  initialX = e.clientX - offsetX
-  initialY = e.clientY - offsetY
+function startMove (event) {
+  initialX = event.clientX - offsetX
+  initialY = event.clientY - offsetY
 
-  if (e.target === dragWindow) {
+  if (event.target === dragWindow) {
     active = true
   }
 }
 
-function dragEnd (e) {
+function stopMove () {
   initialX = currentX
   initialY = currentY
 
   active = false
 }
 
-function drag (e) {
+function drag (event) {
   if (active) {
-    e.preventDefault()
+    event.preventDefault()
 
-    currentX = e.clientX - initialX
-    currentY = e.clientY - initialY
+    currentX = event.clientX - initialX
+    currentY = event.clientY - initialY
 
     offsetX = currentX
     offsetY = currentY
 
-    setTranslate(currentX, currentY, dragWindow)
+    setTranslate(currentX, currentY, container)
   }
 }
 
 function setTranslate (xPos, yPos, elem) {
   elem.style.transform = 'translate3d(' + xPos + 'px, ' + yPos + 'px, 0)'
 }
+
+// let dragWindow = document.querySelector('#moveme')
+// let dragPoint = document.querySelector('#moveheader')
+
+// let currentX
+// let currentY
+// let initialX
+// let initialY
+// let offsetX = 0
+// let offsetY = 0
+
+// dragPoint.addEventListener('mousedown', function startDrag (event) {
+//   initialX = event.clientX
+//   initialY = event.clientY
+
+//   dragPoint.addEventListener('mousemove', function drag (event) {
+//     event.preventDefault()
+
+//     currentX = initialX - event.clientX
+//     currentY = initialY - event.clientY
+
+//     initialX = event.clientX
+//     initialY = event.clientY
+
+//     stopDrag(currentX, currentY, dragWindow)
+//   })
+// })
+
+// function stopDrag (posX, posY, elem) {
+//   dragWindow.style.top = (dragWindow.offsetTop - currentY) + 'px'
+//   dragWindow.style.left = (dragWindow.offsetLeft - currentX) + 'px'
+// }
